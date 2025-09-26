a8d4b5988e1ebbae5560d715b06df8466d314218
Adithya Sreeni <adithyasreeni029@gmail.com>
2025-09-26 10:09:57 +0530

    aggregate functions.

--- other_queries.sql
+++ other_queries.sql
@@ -3,4 +3,4 @@ SELECT MIN(price) AS Lowest price,MAX(price) AS Highest price FROM `mobile store
 -- Total Stock
 SELECT SUM(stock) AS Total stock FROM `mobile stores`;
 -- Top 2 most expensive mobiles
-SELECT * fROM `mobile store` ORDER BY price DESC LIMIT 2;
+SELECT * FROM `mobile store` ORDER BY price DESC LIMIT 2;
